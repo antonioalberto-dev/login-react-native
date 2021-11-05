5b3f68e8917a3d3ed73c81e9dc5d18ce8671ac05
Alberto <antonioalbertodsb@gmail.com>
2021-11-05 00:04:17 -0300

    Adicionando animações

--- App.js
+++ App.js
@@ -1,17 +1,80 @@
-import React from 'react';
-import { View, KeyboardAvoidingView, Image, TextInput, TouchableOpacity, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+  Keyboard, View, KeyboardAvoidingView, Image, TextInput,
+  TouchableOpacity, Text, StyleSheet, Animated
+} from 'react-native';
 
 export default function App() {
+
+  const [offset] = useState(new Animated.ValueXY({ x: 0, y: 80 }));
+  const [opacity] = useState(new Animated.Value(0));
+  const [logo] = useState(new Animated.ValueXY({ x: 130, y: 155 }))
+
+  useEffect(() => {
+    keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', keyboardDidShow);
+    keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', keyboardDidHide);
+    Animated.parallel([
+      Animated.spring(offset.y, {
+        toValue: 0,
+        speed: 4,
+        bounciness: 30,
+      }),
+      Animated.timing(opacity, {
+        toValue: 1,
+        duration: 300,
+      })
+    ]).start();
+  }, []);
+
+  function keyboardDidShow() {
+    Animated.parallel([
+      Animated.timing(logo.x, {
+        toValue: 55,
+        duration: 100,
+      }),
+      Animated.timing(logo.y, {
+        toValue: 65,
+        duration: 100,
+      }),
+    ]).start();
+  }
+
+  function keyboardDidHide() {
+    Animated.parallel([
+      Animated.timing(logo.x, {
+        toValue: 130,
+        duration: 100,
+      }),
+      Animated.timing(logo.y, {
+        toValue: 155,
+        duration: 100,
+      }),
+    ]).start();
+  }
+
   return (
     <>
       <KeyboardAvoidingView style={styles.background}>
         <View style={styles.containerLogo}>
-          <Image
+          <Animated.Image
+            style={{
+              width: logo.x,
+              height: logo.y,
+            }}
             source={require('./assets/logo.png')}
           />
         </View>
 
-        <View style={styles.container}>
+        <Animated.View
+          style={[
+            styles.container, {
+              opacity: opacity,
+              transform: [
+                { translateY: offset.y }
+              ]
+            }
+          ]}
+        >
           <TextInput
             style={styles.input}
             placeholder="Email"
@@ -36,7 +99,7 @@ export default function App() {
               Criar conta gratuita
             </Text>
           </TouchableOpacity>
-        </View>
+        </Animated.View>
       </KeyboardAvoidingView>
     </>
   );
@@ -66,18 +129,18 @@ const styles = StyleSheet.create({
     marginBottom: 15,
     color: '#222',
     fontSize: 17,
-    borderRadius: 8,
+    borderRadius: 15,
     padding: 10,
   },
   btnSubmit: {
     backgroundColor: '#009fff',
-    width: '60%',
+    width: '80%',
     height: 45,
     alignItems: 'center',
     justifyContent: 'center',
     padding: 12,
     marginBottom: 15,
-    borderRadius: 40,
+    borderRadius: 15,
     fontSize: 17,
   },
   textSubmit: {
